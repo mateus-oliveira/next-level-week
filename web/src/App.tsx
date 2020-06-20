224f067f6bf48e8cc0a056bff3019ad655c8f79c
mateus-oliveira <matews5522@gmail.com>
2020-06-20 20:48:49 -0300

    Web started

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world</h1>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Hello world</h1>
+            </header>
+        </div>
+    );
 }
 
 export default App;
